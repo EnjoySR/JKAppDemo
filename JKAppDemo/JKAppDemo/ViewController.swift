503631fb4034fec91c5a0bc0b2b201d22b9c8d63
EnjoySR <yinqiaoyin@gmail.com>
2016-10-11 13:05:07 +0800

    项目初始化

---
+++ JKAppDemo/JKAppDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  JKAppDemo
+//
+//  Created by EnjoySR on 2016/10/11.
+//  Copyright © 2016年 EnjoySR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
